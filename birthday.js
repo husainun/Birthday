abbe04e3f792a7ea60afa027e3111193a8a03449
Uzma <uzmanhusain@yahoo.com>
2018-02-24 11:05:15 -0600

    First commit

---
+++ birthday.js
@@ -0,0 +1,17 @@
+var daysUntilMyBirthday = 60;
+while (daysUntilMyBirthday > 0)
+{
+  if (daysUntilMyBirthday > 30)
+  {
+    console.log (daysUntilMyBirthday + "days,  Such a long time to my Birthday...")
+  }
+  else
+  if (daysUntilMyBirthday <= 30)
+  {
+    console.log (daysUntilMyBirthday + "DAYS UNTIL MY BIRTHDAY!!!!")
+  }
+    daysUntilMyBirthday = daysUntilMyBirthday - 1;
+}
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+console.log("Hurray it is my Birthday !!! ")
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
